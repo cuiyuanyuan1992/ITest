f10f9ad982a673809f2e448760f42bc50c6b7f86
cuiyuanyuan <821380792@qq.com>
2021-09-15 16:31:20 +0800

    update

--- src/test/java/com/qa/test/DemoTest.java
+++ src/test/java/com/qa/test/DemoTest.java
@@ -11,13 +11,13 @@ import org.testng.annotations.Test;
  */
 public class DemoTest extends BaseCommon {
 
-    @Test
+    @Test(description = "这是一个测试不通过的case")
     public void testcase1(){
         System.out.println("testcase1：这是一个测试不通过的case");
         System.out.println("username："+properties.getProperty("username"));
         Assert.assertTrue(false);
     }
-    @Test
+    @Test(description = "这是一个测试通过的case")
     public void testcase2(){
         System.out.println("testcase2：这是一个测试通过的case");
         TestService testService = new TestService();
